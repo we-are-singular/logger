0a66c582033ea64081e85e72a187bb22d708022a
Jonathan Tavares <jonathan@wearesingular.com>
2024-03-23 01:19:07 +0000

    style: adds markdown preset

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,19 @@
 const { config, presets, overrides } = require("@we-are-singular/eslint-config")
 
-module.exports = config(presets.typescript, presets.prettier, {
-  overrides: [
-    overrides.namingConvention(["src/**/*.ts"], []),
-    // overrides.typescript(["src/**/*.ts"], {
-    //   parserOptions: {
-    //     project: "./tsconfig.json",
-    //   },
-    // }),
-  ],
-  ignorePatterns: [".eslintrc.*", "node_modules"],
-})
+module.exports = config(
+  //
+  presets.typescript,
+  presets.prettier,
+  presets.markdown,
+  {
+    overrides: [
+      overrides.namingConvention(["src/**/*.ts"], []),
+      // overrides.typescript(["src/**/*.ts"], {
+      //   parserOptions: {
+      //     project: "./tsconfig.json",
+      //   },
+      // }),
+    ],
+    ignorePatterns: [".eslintrc.*", "node_modules"],
+  }
+)
